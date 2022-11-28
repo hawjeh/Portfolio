cf3cf1ea53e6fddfeb6723f9b6deecf106a82ad8
hawjeh <hawjeh.lee@websparks.sg>
2022-11-28 16:11:14 +0800

    Added test ads

--- src/hooks/useGAdSenseScript.js
+++ src/hooks/useGAdSenseScript.js
@@ -8,8 +8,24 @@ const useGAdSenseScript = (id) => {
     script.crossOrigin = 'anonymous';
     document.body.appendChild(script);
 
+    const script2 = document.createElement('script');
+    script2.innerHTML =
+      `
+      <ins class="adsbygoogle"
+      style="display:block"
+      data-ad-client="${id}"
+      data-ad-slot="8929561319"
+      data-ad-format="auto"
+      data-full-width-responsive="true"></ins>
+      <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
+    `;
+    document.body.appendChild(script2);
+
     return () => {
       document.body.removeChild(script);
+      document.body.removeChild(script2);
     }
   }, [id]);
 };
