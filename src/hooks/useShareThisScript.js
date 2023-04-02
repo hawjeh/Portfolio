913e8bfa3a476a17c77b0f20feaa919583478e54
hawjeh <hawjeh.lee@websparks.sg>
2023-04-02 20:17:11 +0800

    update sharethis

--- src/hooks/useShareThisScript.js
+++ src/hooks/useShareThisScript.js
@@ -3,8 +3,9 @@ import { useEffect } from 'react';
 const useShareThisScript = (id) => {
   useEffect(() => {
     const script = document.createElement('script');
+    script.type = 'text/javascript';
     script.src = `https://platform-api.sharethis.com/js/sharethis.js#property=${id}&product=inline-share-buttons`;
-    script.async = true;
+    script.async = 'async';
     document.head.appendChild(script);
 
     return () => {
