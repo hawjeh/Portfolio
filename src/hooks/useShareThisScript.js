701a0f5412aedba89c2d7f41778c6bb42eb6ced4
hawjeh <hawjeh.lee@websparks.sg>
2023-04-02 19:17:08 +0800

    fix path

--- src/hooks/useShareThisScript.js
+++ src/hooks/useShareThisScript.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 const useShareThisScript = (id) => {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = `https//platform-api.sharethis.com/js/sharethis.js#property=${id}&product=inline-share-buttons&source=platform`;
+    script.src = `https://platform-api.sharethis.com/js/sharethis.js#property=${id}&product=inline-share-buttons&source=platform`;
     script.async = true;
     document.head.appendChild(script);
 
